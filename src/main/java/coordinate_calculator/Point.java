8faf8813637790a3f9f246a7baee13f736bc8964
by-gramm <ready2start@naver.com>
2022-09-29 16:07:11 +0900

    Feat: 좌표 생성시 예외 처리 (좌표값은 0 이상 24 이하)

--- src/main/java/coordinate_calculator/Point.java
+++ src/main/java/coordinate_calculator/Point.java
@@ -2,6 +2,9 @@ package coordinate_calculator;
 
 public class Point {
 
+    public static final int MIN_COORDINATE_VALUE = 0;
+    public static final int MAX_COORDINATE_VALUE = 24;
+
     private final int x;
     private final int y;
 
@@ -11,6 +14,10 @@ public class Point {
     }
 
     public static Point of(int x, int y) {
+        if (Math.max(x, y) > MAX_COORDINATE_VALUE || Math.min(x, y) < MIN_COORDINATE_VALUE) {
+            throw new IllegalArgumentException("좌표값은 0 이상 24 이하의 수만 가능합니다.");
+        }
+
         return new Point(x, y);
     }
 }
--- src/test/java/coordinate_calculator/PointTest.java
+++ src/test/java/coordinate_calculator/PointTest.java
@@ -1,6 +1,8 @@
 package coordinate_calculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -10,5 +12,20 @@ public class PointTest {
     void 좌표_생성() {
         Point point = Point.of(10, 20);
         assertThat(point).isInstanceOf(Point.class);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"10:-1:true", "24:24:false", "0:0:false", "10:25:true"}, delimiter = ':')
+    void 좌표값_예외_처리(int x, int y, boolean isException) {
+        if (isException) {
+            assertThatThrownBy(() -> Point.of(x, y))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("좌표값은 0 이상 24 이하의 수만 가능합니다.");
+        }
+
+        if (!isException) {
+            assertThatCode(() -> Point.of(x, y))
+                    .doesNotThrowAnyException();
+        }
     }
 }
