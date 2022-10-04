302b16c6748060211f78c038c066f04e3703bf61
by-gramm <ready2start@naver.com>
2022-10-05 01:11:21 +0900

    Refactor: 다형성 제거 실습

--- src/main/java/coordinate/FigureFactory.java
+++ src/main/java/coordinate/FigureFactory.java
@@ -1,21 +1,29 @@
 package coordinate;
 
+import coordinate.creator.FigureCreator;
+import coordinate.creator.LineCreator;
+import coordinate.creator.RectangleCreator;
+import coordinate.creator.TriangleCreator;
+
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class FigureFactory {
+
+    private static final ConcurrentHashMap<Integer, FigureCreator> shapeMap = new ConcurrentHashMap<>();
+
+    static {
+        shapeMap.put(Line.LINE_POINT_SIZE, new LineCreator());
+        shapeMap.put(Triangle.TRIANGLE_POINT_SIZE, new TriangleCreator());
+        shapeMap.put(Rectangle.RECTANGLE_POINT_SIZE, new RectangleCreator());
+    }
+
     static Figure getInstance(List<Point> points) {
-        if (points.size() == Line.LINE_POINT_SIZE) {
-            return new Line(points);
+        if (!shapeMap.containsKey(points.size())) {
+            throw new IllegalArgumentException("유효하지 않은 도형입니다.");
         }
 
-        if (points.size() == Triangle.TRIANGLE_POINT_SIZE) {
-            return new Triangle(points);
-        }
-
-        if (points.size() == Rectangle.RECTANGLE_POINT_SIZE) {
-            return new Rectangle(points);
-        }
-
-        throw new IllegalArgumentException("유효하지 않은 도형입니다.");
+        FigureCreator creator = shapeMap.get(points.size());
+        return creator.create(points);
     }
 }
